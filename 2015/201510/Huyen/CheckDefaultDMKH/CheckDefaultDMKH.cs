90d65f4ca3b7a7c9cb91d102244e5ffff4e6eb48
HuyenNguyenit <huyennguyenttkt@gmail.com>
2015-11-09 14:19:31 +0700

    Sửa lại CheckDefault DMKH

--- 2015/201510/Huyen/CheckDefaultDMKH/CheckDefaultDMKH.cs
+++ 2015/201510/Huyen/CheckDefaultDMKH/CheckDefaultDMKH.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Plugins;
 using System.Data;
+using CDTDatabase;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid;
+using DevExpress.XtraEditors;
+
 
 namespace CheckDefaultDMKH
 {
     class CheckDefaultDMKH : ICControl
     {
-
-        private readonly string[] FORM_NAME = { "Khách hàng", "Nhà cung cấp", "Nhân viên"};
-        private readonly string[] COLUMN_NAME = { "IsKH", "IsNCC", "IsNV" };
+        private readonly string[] COLUMN_NAME = { "isKH = 1", "isNCC = 1", "isNV = 1" };
         private DataCustomFormControl _data;
         private InfoCustomControl _info = new InfoCustomControl(IDataType.SingleDt);
+        private Database _dbData = Database.NewDataDatabase();
+
+        //[Lệ Huyền] Created [28/10/2015]
+        // Check mặc định loại nhân viên, KH, NCC
 
         #region ICControl Members
 
@@ -31,10 +39,14 @@ namespace CheckDefaultDMKH
             _data.FrmMain.Load += new EventHandler(FormLoad);
         }
 
+        // Update [09/11/2015]
+        // Check mặc định loại nhân viên, KH, NCC
+
         private void FormLoad(object sender, EventArgs e)
         {
             BindingSource bindingSource = null;
             DataRow mtRowView = null;
+
             bindingSource = _data.BsMain as BindingSource;
             if (bindingSource == null || bindingSource.Current == null)
                 return;
@@ -42,14 +54,20 @@ namespace CheckDefaultDMKH
             mtRowView = (bindingSource.Current as DataRowView).Row;
             if (mtRowView == null || mtRowView.RowState == DataRowState.Deleted)
                 return;
-            string formName = _data.FrmMain.Text;
-
-            for (int i = 0; i < FORM_NAME.Length; i++)
+            string _strCheck = _data.DrTable["ExtraSql"].ToString();
+            if (!COLUMN_NAME.Contains(_strCheck))
+                return;
+            if (_strCheck == "isKH = 1")
             {
-                if ((formName == FORM_NAME[i]))
-                {
-                    mtRowView[COLUMN_NAME[i]] = true;
-                }
+                mtRowView["IsKH"] = true;
+            }
+            else if (_strCheck == "isNV = 1")
+            {
+                mtRowView["IsNV"] = true;
+            }
+            else
+            {
+                mtRowView["IsNCC"] = true;
             }
         }
         #endregion
