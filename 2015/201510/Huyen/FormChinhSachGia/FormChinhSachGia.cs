043a17e832f283f53304cdf5133792e45430ab8f
HuyenNguyenit <huyennguyenttkt@gmail.com>
2015-11-11 09:29:51 +0700

    Source loại ICForm màn hình chức năng chọn bảng giá vật tư theo khách hàng

---
+++ 2015/201510/Huyen/FormChinhSachGia/FormChinhSachGia.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Plugins;
+using DevExpress.XtraEditors;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Views.Base;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraGrid;
+using System.Data;
+using CDTLib;
+using CDTDatabase;
+
+
+namespace FormChinhSachGia
+{
+    class FormChinhSachGia : ICForm
+    {
+        DataCustomFormControl _data;
+        List<InfoCustomForm> _lstInfo = new List<InfoCustomForm>();
+        private Database _dbData =Database.NewDataDatabase();
+        public FormChinhSachGia()
+        {
+            InfoCustomForm info = new InfoCustomForm(IDataType.MasterDetailDt, 10002, "Chọn chính sách giá bán theo khách hàng", "Select policies by customer price", "MT32");
+            _lstInfo.Add(info);
+        }
+
+        //[Lệ Huyền] Tạo mới [05/11/2015]
+        // Xem và chọn áp phiếu điều chuyển kho để xuất hóa đơn
+        #region ICForm Members
+
+        public DataCustomFormControl Data
+        {
+            set { _data = value; }
+        }
+        public List<InfoCustomForm> LstInfo
+        {
+            get { return _lstInfo; }
+        }
+        #endregion
+
+        public void Execute(int menuID)
+        {
+            if (menuID == _lstInfo[0].MenuID)
+            {
+                GridView gvDetail = (_data.FrmMain.Controls.Find("gcMain", true)[0] as GridControl).MainView as GridView;
+                if (_data.BsMain.Current == null)
+                {
+                    string msg = "Chưa đủ số liệu để tra phiếu nhập";
+                    if (Config.GetValue("Language").ToString() == "1")
+                        msg = UIDictionary.Translate(msg);
+                    XtraMessageBox.Show(msg);
+                    return;
+                }
+                DataRow drCurMaster = (_data.BsMain.Current as DataRowView).Row;
+                DataRow drCurDetail = gvDetail.GetDataRow(gvDetail.FocusedRowHandle);
+                if (drCurMaster["MaKH"].ToString() == "")
+                {
+                    string msg = "Cần có mã khách hàng để tra phiếu nhập";
+                    if (Config.GetValue("Language").ToString() == "1")
+                        msg = UIDictionary.Translate(msg);
+                    XtraMessageBox.Show(msg);
+                    return;
+                }
+                FrmCSGTheoKH frm = new FrmCSGTheoKH(drCurMaster, drCurDetail);
+                DialogResult dialogResult = frm.ShowDialog();
+                if (dialogResult == DialogResult.OK
+                        && gvDetail != null
+                        && frm.Result != null
+                        && frm.Result.Count > 0)
+                
+                {
+                    // Xóa dữ liệu trên lưới
+                    for (int i = gvDetail.RowCount - 1; i >= 0; i--)
+                    {
+                        gvDetail.DeleteRow(i);
+                    }
+                    //Lấy dữ liệu
+                    foreach (DataRowView dataRowView in frm.Result)
+                    {
+                        string strTemp1 = dataRowView["MaVT"].ToString();
+                        string strTemp2 = dataRowView["MaDVT"].ToString();
+                        string _sqlView = @"Select MaVT, MaDVT from wTonKhoTucThoi Where MaVT = '" + strTemp1+ "' and MaDVT = '"+ strTemp2 + "'";
+                        DataTable dtView = new DataTable();
+                        dtView = _dbData.GetDataTable(_sqlView);
+                        dtView.PrimaryKey = new DataColumn[] {dtView.Columns["MaVT"] };
+                    //Kiểm tra xem MaVT và MaDVT load lên từ bảng DMChinhSachGia có chứa trong View Tonkhotucthoi
+                        if (dtView.Rows.Count > 0)
+                        {
+                            gvDetail.AddNewRow();
+                            gvDetail.SetFocusedRowCellValue(gvDetail.Columns["MaVT"], dataRowView["MaVT"]);
+                            gvDetail.SetFocusedRowCellValue(gvDetail.Columns["TenVT"], dataRowView["TenVT"]);
+                            gvDetail.SetFocusedRowCellValue(gvDetail.Columns["MaDVT"], dataRowView["MaDVT"]);
+                            gvDetail.SetFocusedRowCellValue(gvDetail.Columns["Gia"], dataRowView["Gia"]);
+                        }
+                        else
+                        {
+                            string msg = "Vật tư chưa nhập kho, vui lòng chon vật tư khác hoặc nhập kho vật tư hiện tại";
+                            if (Config.GetValue("Language").ToString() == "1")
+                                msg = UIDictionary.Translate(msg);
+                            XtraMessageBox.Show(msg);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 2015/201510/Huyen/FormChinhSachGia/FrmCSGTheoKH.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CDTControl;
+using CDTDatabase;
+using CDTLib;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraGrid.Views.Base;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+
+
+namespace FormChinhSachGia
+{
+    public partial class FrmCSGTheoKH : DevExpress.XtraEditors.XtraForm
+    {
+        #region "Variables"
+
+        private DataRow _drCurMaster;
+        private DataRow _drCurDetail;
+        private DataTable _dtVT = null;
+        public DataView Result = null;
+        private Database _dbData = Database.NewDataDatabase();
+       // GridColumn column;
+      //  RepositoryItemCheckEdit edit;
+
+        #endregion
+
+        //[Lệ Huyền] Tạo mới [05/11/2015]
+        // Tạo mới màn hình chính sách giá bán vật tư theo khách hàng
+        public FrmCSGTheoKH(DataRow drCurMaster, DataRow drCurDetail)
+        {
+            InitializeComponent();
+            _drCurMaster = drCurMaster;
+            _drCurDetail = drCurDetail;
+        }
+
+        #region "Method"
+
+        /// <summary>
+        /// Init
+        /// </summary>
+        private void InitData()
+        {
+            string sql = @"SELECT d.MaKH,  k.TenKH,  d.MaVT, v.TenVT,  d.MaDVT,  d.Gia
+                            FROM DMChinhSachGia d inner join DMKH k on d.MaKH=k.MaKH
+                                   inner join DMVT v on d.MaVT=v.MaVT
+                            Where d.MaKH = '" + _drCurMaster["MaKH"].ToString() + "'";
+             _dtVT = _dbData.GetDataTable(sql);
+             DataColumn column = new DataColumn("Check", typeof(Boolean));
+            column.DefaultValue = false;
+            _dtVT.Columns.Add(column);
+            gcPN.DataSource = _dtVT;
+            gvPN.ExpandAllGroups();
+            gvPN.BestFitColumns();
+        }
+        /// <summary>
+        /// Format column
+        /// </summary>
+        private void FormatColumnString()
+        {
+            clGia.DisplayFormat.FormatString = FormatString.GetReportFormat("Gia");
+        }
+        /// <summary>
+        /// Check/Uncheck All
+        /// </summary>
+        public void SelectAll(bool isSelectAll)
+        {
+            for (int i = 0; i < gvPN.DataRowCount; i++)
+            {
+                DataRow row = gvPN.GetDataRow(i) as DataRow;
+                row["Check"] = isSelectAll;
+            }
+        }
+        /// <summary>
+        /// Handling node Save
+        /// </summary>
+        private void HandlingSave()
+        {
+            DataView dv = new DataView(_dtVT);
+            dv.RowFilter = "Check = true";
+            if (dv.Count > 0)
+            {
+                Result = dv;
+                this.DialogResult = DialogResult.OK;
+                this.Close();         
+            }
+            else
+            {
+                string msg = "Phải chọn ít nhất một Vật tư!";
+                if (Config.GetValue("Language").ToString() == "1")
+                    msg = UIDictionary.Translate(msg);
+                XtraMessageBox.Show(msg);
+                return;  
+            }
+        }
+        #endregion
+
+
+        #region "Event"
+
+        /// <summary>
+        /// Form Load
+        /// </summary>
+        private void FormCSGTheoKH_Load(object sender, EventArgs e)
+        {
+            FormatColumnString();
+            InitData();
+            this.KeyPreview = true;
+            this.KeyUp += new KeyEventHandler(FrmCSGTheoKH_KeyUp);
+        }
+        ///<summary>
+        /// Handling KeyUp
+        /// </summary>
+        private void FrmCSGTheoKH_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F12)
+            {
+                gvPN.FocusedRowHandle++;
+                HandlingSave();
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            
+        }
+        /// <summary>
+        /// Handling node Cancel
+        /// </summary>
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+        /// <summary>
+        /// Save
+        /// </summary>
+        private void btOk_Click(object sender, EventArgs e)
+        {
+            HandlingSave();
+        }
+        //
+        /// <summary>
+        /// Handling Check/Uncheck All
+        /// </summary>
+        private void CheckAll_CheckedChanged(object sender, EventArgs e)
+        {
+            SelectAll(CheckAll.Checked);
+        }
+
+        #endregion
+      
+    }
+}
