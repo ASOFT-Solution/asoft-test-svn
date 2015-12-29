2d61004ad92b8a689b51ad379f3b09dcac2c89ca
HuyenNguyenit <huyennguyenttkt@gmail.com>
2015-12-29 14:32:08 +0700

    Scripts các màn hình bảng kê thuế TTDB mua và, và scripts chỉnh sửa các màn hình bảng kê thuế TTDB bán ra

--- 2015/201510/DATABASE/02_STDPRO/9999_NewScripts/01_TABLES/89_MTTDBOut.sql
+++ 2015/201510/DATABASE/02_STDPRO/9999_NewScripts/01_TABLES/89_MTTDBOut.sql
@@ -1,3 +1,6 @@
+ALTER TABLE MTTDBOut
+ALTER COLUMN KyBKBRTTDB int NULL
+
 if not exists (select * from syscolumns col inner join sysobjects tab 
            On col.id = tab.id where tab.name = 'MTTDBOut'  and col.name = 'DeclareType')
 BEGIN
