458f1d26333f7e4d9179bdecc221d3828f574481
caothiphuong <phuongcao.buh@gmail.com>
2016-01-14 12:04:24 +0700

    update du liệu mới sau khi chỉnh sửa store và Input check

--- 2015/201512/10_DOCUMENT/13_DETAIL_DESIGN/Danh muc so do tuyen/CP0143.sql
+++ 2015/201512/10_DOCUMENT/13_DETAIL_DESIGN/Danh muc so do tuyen/CP0143.sql
@@ -7,31 +7,30 @@ SET ANSI_NULLS ON
 GO
 
 -- <Summary>
----- In báo cáo Sơ đồ tuyến màn hình truy vấn - CF0143 [Customize HoangTran]
+---- In báo cáo Sơ đồ tuyến màn hình truy vấn - CF0143 
 -- <History>
 ---- Create on 13/01/2016 by Thị Phượng 
 ---- Modified on ... by 
 -- <Example>
 /* 
-CP0143 @DivisionID ='CTY', @RouteIDList = '01'',''02'
+CP0143 @DivisionID ='CTY', @RouteID = '01'',''02'
  */
 CREATE PROCEDURE [dbo].[CP0143] 	
 	@DivisionID NVARCHAR(50),
-	@RouteIDList NVARCHAR(MAX)
+	@RouteID NVARCHAR(MAX)
 AS
 DECLARE @sSQL1 NVARCHAR(MAX)
-SET @RouteIDList = ISNULL(@RouteIDList,'')
+SET @RouteID = ISNULL(@RouteID,'')
 -- Load dữ liệu cho lưới thông tin giao hàng
 SET @sSQL1 = '
-SELECT CT43.RouteID, CT43.RouteName, CT44.StationOrder, AT47.ObjectID,
+SELECT CT43.RouteID, CT43.RouteName, CT44.StationOrder, AT02.ObjectID,
        AT02.ObjectName, CT44.StationID, CT41.StationName, CT41.[Address], CT44.Notes
 FROM CT0143 CT43
 INNER JOIN CT0144 CT44 ON CT44.DivisionID = CT43.DivisionID AND CT44.RouteID = CT43.RouteID
 LEFT JOIN CT0141 CT41 ON CT41.DivisionID = CT43.DivisionID AND CT41.StationID = CT44.StationID
-LEFT JOIN AT0047 AT47 ON AT47.DivisionID = CT41.DivisionID AND AT47.StationID = CT41.StationID
-LEFT JOIN AT1202 AT02 ON AT02.DivisionID = AT47.DivisionID AND AT02.ObjectID = AT47.ObjectID
-WHERE CT43.DivisionID = '''+@DivisionID+''' AND CT43.RouteID IN ('''+@RouteIDList+''')
-ORDER BY CT43.RouteID, CT43.StationOrder, CT44.StationID, AT47.ObjectID
+LEFT JOIN AT1202 AT02 ON AT02.DivisionID = CT43.DivisionID 
+WHERE CT43.DivisionID = '''+@DivisionID+''' AND CT43.RouteID IN ('''+@RouteID+''')
+ORDER BY CT43.RouteID, CT44.StationOrder, CT44.StationID, AT02.ObjectID
 	'
 EXEC (@sSQL1)
 --PRINT (@sSQL1)
