7021f25c019b2a29884a2d6086b8b868714046fb
MsThanhLam <33396604+MsThanhLam@users.noreply.github.com>
2017-11-07 16:05:23 +0700

    Lam commit bài tập

---
+++ 2017/201711/BaiTap/BaiTap/Models/Search.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap.Models
+{
+    public class Search
+    {
+        private BaiTapKendoEntities db = new BaiTapKendoEntities();
+        public List<string> Search_hv(string dtSearch)
+        {
+            IQueryable<HoiVien> model = db.HoiVien;
+            return model.Where(x => x.DivisionID.Contains(dtSearch)).Select(x => x.DivisionID).ToList();
+        }
+    }
+}
